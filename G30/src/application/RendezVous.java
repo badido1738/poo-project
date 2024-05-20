d3713a862444786f2f0bf38ba8eec2551755fbd8
ABBAD TECHNOLOGY <ABBAD TECHNOLOGY@DESKTOP-AO0VNVC>
2024-05-20 01:04:09 +0100

    3RD TRY

---
+++ G30/src/application/RendezVous.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Date;
+
+public class RendezVous {
+	  private Patient patient;
+	    private Date date;
+
+	    public RendezVous(Patient patient, Date date) {
+	        this.patient = patient;
+	        this.date = date;
+	    }
+
+	    // Getters et setters
+	    public Patient getPatient() {
+	        return patient;
+	    }
+
+	    public Date getDate() {
+	        return date;
+	    }
+}
+
